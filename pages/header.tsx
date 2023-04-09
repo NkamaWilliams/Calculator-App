54a24727796ee5a74ac13a1812365c72e9b5f072
Nkama Williams <williamsnkama@gmail.com>
2023-04-09 09:40:10 +0100

    toggle v0.1

--- pages/header.tsx
+++ pages/header.tsx
@@ -1,6 +1,30 @@
 import head from '@/styles/header.module.css'
+import styles from './styles'
+import { useState } from 'react'
 
 export default function Header(){
+    const [headState, setHead] = useState({
+      toggleValue: 0,
+      numOfValues: 3
+    })
+
+    const toggleType = [1, 2, 3].map(i => <span className={head.toggleType}>{i}</span>)
+
+    const selectButton = [1, 2, 3].map(i => headState.toggleValue === i-1? <div className={head.toggleActive}></div> : <div></div>)
+    
+    function clickButton(){
+      setHead(prev => {return {
+        ...prev,
+        toggleValue: (headState.toggleValue + 1) % headState.numOfValues
+      }});
+      console.log(styles[0]);
+      // changeStyles();
+    }
+
+    // function changeStyles(){
+    //   const rt = document.querySelector(':root');
+    //   let rs = getComputedStyle(rt);
+    // }
     return(
         <header className={head.container}>
             <h2 className={head.h2}>calc</h2>
@@ -10,15 +34,11 @@ export default function Header(){
 
               <div className={head.toggleSelection}>
                 <section className={head.typeContainer}>
-                  <span className={head.toggleType}>1</span>
-                  <span className={head.toggleType}>2</span>
-                  <span className={head.toggleType}>3</span>
+                  {toggleType}
                 </section>
 
-                <section className={head.selection}>
-                  <div className={head.toggleActive}></div>
-                  <div></div>
-                  <div></div>
+                <section onClick={clickButton} className={head.selection}>
+                  {selectButton}
                 </section>
               </div>
             </div>
