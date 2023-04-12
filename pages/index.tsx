ebd3a3a65b3254ff56ce172babc0fb7d037fcf07
Nkama Williams <williamsnkama@gmail.com>
2023-04-12 19:05:13 +0100

    screen and keypad 1.0

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import styles from "@/pages/styles.js"
+import keyStyles from '@/styles/Keypad.module.css'
 import {useState} from 'react'
 import Image from 'next/image';
 import Screen from './screen';
@@ -11,7 +12,7 @@ import Calc from '@/styles/Calc.module.css';
 
 // const inter = Inter({ subsets: ['latin'] })
 
-//MAIN BRANCH
+//SCREEN BRANCH
 
 export default function Home() {
   const [main, setMain] = useState({
@@ -19,6 +20,28 @@ export default function Home() {
     input: [],
     command: ""
   })
+
+  const buttons = [
+    {7: '7', 8:'8', 9: '9', del: 'DEL'},
+    {4: '4', 5: '5', 6: '6', plus: '+'},
+    {1: '1', 2: '2', 3: '3', minus: '-'},
+    {dot: '.', 0: '0', div: '/', mul: 'x'},
+    {reset: 'RESET', equals: '='}
+  ]
+
+  const key = buttons.map(i => {
+    let val = Object.values(i);
+    const row = val.map(x => {
+      switch(x){
+        case "DEL": return <span className={keyStyles.del}>{x}</span>
+        case "RESET": return <span className={keyStyles.reset}>{x}</span>
+        case "=": return <span className={keyStyles.equals}>{x}</span>
+        default: return <span>{x}</span>
+      }
+    });
+    return <div className={keyStyles.key}>{row}</div>;
+  })
+  
   return (
     <>
       <Head>
@@ -28,116 +51,14 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
         <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=League+Spartan'/>
       </Head>
-      {/* <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
-      </main> */}
-
 
       <main className={Calc.main1}>
         <div className={Calc.container}>
           <Header />
           <Screen disp={main.disp}/>
+          <section className={keyStyles.keypad}>
+            {key}
+          </section>
         </div>
       </main>
 
--- pages/styles.js
+++ pages/styles.js
@@ -10,8 +10,11 @@ const styles = [
         key1sh2: 'hsl(6, 70%, 34%)',
         key1bg3: 'hsl(30, 25%, 89%)',
         key1sh3: 'hsl(28, 16%, 65%)',
-        text11: 'hsl(0, 0%, 100%)',
-        text12: 'hsl(0, 0%, 100%)'
+        headtext: 'hsl(0, 0%, 100%)',
+        keytext1: 'hsl(221, 14%, 31%)',
+        keytext2: 'hsl(0, 0%, 100%)',
+        keytext3: 'hsl(0, 0%, 100%)',
+        keypad: 'hsl(223, 31%, 20%)'
     },
 
     {
@@ -25,8 +28,11 @@ const styles = [
         key1sh2: 'hsl(25, 99%, 27%)',
         key1bg3: 'hsl(45, 7%, 89%)',
         key1sh3: 'hsl(35, 11%, 61%)',
-        text11: 'hsl(60, 10%, 19%)',
-        text12: 'hsl(0, 0%, 100%)'
+        headtext: 'hsl(60, 10%, 19%)',
+        keytext1: 'hsl(60, 10%, 19%)',
+        keytext2: 'hsl(0, 0%, 100%)',
+        keytext3: 'hsl(0, 0%, 100%)',
+        keypad: 'hsl(0, 5%, 81%)'
     },
 
     {
@@ -34,14 +40,17 @@ const styles = [
         bg1: 'hsl(268, 75%, 9%)',
         togbg1: 'hsl(268, 71%, 12%)',
         screenbg: 'hsl(281, 89%, 26%)',
-        key1bg1: 'hsl(285, 91%, 52%)',
-        key1sh1: 'hsl(176, 100%, 44%)',
-        key1bg2: 'hsl(177, 92%, 70%)',
-        key1sh2: 'hsl(268, 47%, 21%)',
-        key1bg3: 'hsl(290, 70%, 36%)',
-        key1sh3: 'hsl(52, 100%, 62%)',
-        text11: 'hsl(52, 100%, 62%)',
-        text12: 'hsl(0, 0%, 100%)'
+        key1bg1: 'hsl(281, 89%, 26%)',
+        key1sh1: 'hsl(285, 91%, 52%)',
+        key1bg2: 'hsl(176, 100%, 44%)',
+        key1sh2: 'hsl(177, 92%, 70%)',
+        key1bg3: 'hsl(268, 47%, 21%)',
+        key1sh3: 'hsl(290, 70%, 36%)',
+        headtext: 'hsl(52, 100%, 62%)',
+        keytext1: 'hsl(52, 100%, 62%)',
+        keytext2: 'hsl(0, 0%, 100%)',
+        keytext3: 'hsl(198, 20%, 13%)',
+        keypad: 'hsl(268, 71%, 12%)'
     }
 ]
 
